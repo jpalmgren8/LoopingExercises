7b2c72e2c113b7b036079f8511d50f0988b41e2d
Julian Palmgren <julianpalmgren@gmail.com>
2025-10-02 10:24:33 -0400

    First commit for looping exercises. Using while, do while, and for loops.

---
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int i = 5;
+
+        do {
+            System.out.println("I love java");
+            i--;
+        } while (i > 0);
+
+    }
+}
---
+++ src/main/java/com/pluralsight/LoopingExercises.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class LoopingExercises {
+    public static void main(String[] args) {
+
+    }
+}
