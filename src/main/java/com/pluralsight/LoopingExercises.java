2b98970db0a579df2f5a6e65d177ac92a522b49b
Julian Palmgren <julianpalmgren@gmail.com>
2025-10-02 10:41:36 -0400

    Second commit. Added a switch statement in main to call each class to run.

--- src/main/java/com/pluralsight/LoopingExercises.java
+++ src/main/java/com/pluralsight/LoopingExercises.java
@@ -1,7 +1,25 @@
 package com.pluralsight;
 
-public class LoopingExercises {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class LoopingExercises {
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Select which loop you would like to run (1-3): ");
+        int loopSelect = sc.nextInt();
+
+        switch (loopSelect) {
+            case 1:
+                WhileLoop.main(args);
+                break;
+            case 2:
+                ForLoop.main(args);
+                break;
+            case 3:
+                DoWhileLoop.main(args);
+                break;
+        }
     }
 }
